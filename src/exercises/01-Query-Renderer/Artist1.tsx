4dbaba2e0822b8e45a7a418cfb6560f4c9fff503
Steven Hicks <steven.j.hicks@gmail.com>
2021-02-11 14:49:30 -0600

    wip on queryrenderers

--- src/exercises/01-Query-Renderer/Artist1.tsx
+++ src/exercises/01-Query-Renderer/Artist1.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-export const Artist1 = () => {
+export const Artist1 = (props) => {
   const { artistID } = useParams();
 
-  return <h1> artist {artistID} </h1>;
+  return (
+    <div>
+      <h1>{props.artist.name}</h1>
+      <h2>b. {props.artist.birthYear}</h2>
+    </div>
+  );
 };
