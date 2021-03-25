d69a5115210e6ef1a2f6be799f11dd62d9d239e6
Steven Hicks <steven.j.hicks@gmail.com>
2021-03-25 13:44:03 -0500

    chore: fix copy-pasta'd svg prop

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -66,7 +66,7 @@ const Icon: React.FC = ({ children }) => {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 1024 1024"
       height="1em"
       width="1em"
