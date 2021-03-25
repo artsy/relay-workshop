f740af512394b1853f35444c280eafaf3627f953
Steven Hicks <steven.j.hicks@gmail.com>
2021-03-25 16:10:27 -0500

    feat: more fields on artist

--- src/graphql/makeServer.ts
+++ src/graphql/makeServer.ts
@@ -20,8 +20,32 @@ export function makeServer() {
 
 function seedArtists() {
   return [
-    { id: '1', name: 'Andy Warhol', birthYear: 1928 },
-    { id: '2', name: 'Nicolas Party', birthYear: 1980 },
-    { id: '3', name: 'Kehinde Wiley', birthYear: 1977 },
+    {
+      id: '1',
+      name: 'Andy Warhol',
+      birthYear: 1928,
+      bio:
+        'Obsessed with celebrity, consumer culture, and mechanical (re)production, Pop artist Andy Warhol created some of the most iconic images of the 20th century. ',
+      auctionRecord: '$105M',
+      auctionLotsSoldAnnually: 103,
+    },
+    {
+      id: '2',
+      name: 'Nicolas Party',
+      birthYear: 1980,
+      bio:
+        'Swiss artist Nicolas Party, who was the subject of a solo exhibition at Swiss Institute in 2012, is known for applying his bright, graphic patterns onto everything from ceramics to furniture to floors, ceilings, doorways, and walls.',
+      auctionRecord: '$1.35M',
+      auctionLotsSoldAnnually: 31,
+    },
+    {
+      id: '3',
+      name: 'Kehinde Wiley',
+      birthYear: 1977,
+      bio:
+        'Working exclusively in portraiture, Kehinde Wiley fuses traditional formats and motifs with modern modes of representation.',
+      auctionRecord: '$378K',
+      auctionLotsSoldAnnually: 53,
+    },
   ];
 }
