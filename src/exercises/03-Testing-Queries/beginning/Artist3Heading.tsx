533977197bc5933637f42ba8fb03f3eb2daee8f8
Steven Hicks <steven.j.hicks@gmail.com>
2021-05-06 11:09:56 -0500

    chore: capture 'beginning' state

---
+++ src/exercises/03-Testing-Queries/beginning/Artist3Heading.tsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import { createFragmentContainer, graphql } from 'react-relay';
+// import { Artist3Heading_artist } from './__generated__/Artist3Heading_artist.graphql';
+
+// interface Artist3HeadingProps {
+//   artist: Artist3Heading_artist;
+// }
+
+// export const Artist3Heading: React.FC<Artist3HeadingProps> = ({ artist }) => {
+//   return (
+//     <>
+//       <h1>{artist.name}</h1>
+//       <h3>b. {artist.birthYear}</h3>
+//     </>
+//   );
+// };
+
+// export const Artist3HeadingFragmentContainer = createFragmentContainer(
+//   Artist3Heading,
+//   {
+//     artist: graphql`
+//       fragment Artist3Heading_artist on Artist {
+//         name
+//         birthYear
+//       }
+//     `,
+//   }
+// );
---
+++ src/exercises/03-Testing-Queries/beginning/Artist3QueryRenderer.tsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import { graphql, QueryRenderer } from 'react-relay';
+// import { useParams } from 'react-router-dom';
+// import { environment } from '../../relay';
+// import { Artist3FragmentContainer } from './Artist3';
+// import { Artist3QueryRendererQuery } from './__generated__/Artist3QueryRendererQuery.graphql';
+
+// export const Artist3QueryRenderer = () => {
+//   const { artistID } = useParams();
+
+//   return (
+//     <QueryRenderer<Artist3QueryRendererQuery>
+//       environment={environment}
+//       query={graphql`
+//         query Artist3QueryRendererQuery($artistID: ID!) {
+//           artist(id: $artistID) {
+//             ...Artist3_artist
+//           }
+//         }
+//       `}
+//       variables={{ artistID }}
+//       render={({ props }) => {
+//         if (!props || !props.artist) {
+//           return <div>Loading</div>;
+//         }
+//         return <Artist3FragmentContainer artist={props.artist} />;
+//       }}
+//     />
+//   );
+// };
