a7598ddc94156dc674f361b0809b6a1f0e2356a3
Steven Hicks <steven.j.hicks@gmail.com>
2021-07-16 10:32:00 -0500

    chore: add more descriptions to the specs we'll build in ex 3

--- src/exercises/03-Testing-Queries/Artist3Heading.spec.tsx
+++ src/exercises/03-Testing-Queries/Artist3Heading.spec.tsx
@@ -10,53 +10,59 @@ import {
 } from "./Artist3Heading"
 import { Artist3HeadingTestQuery } from "./__generated__/Artist3HeadingTestQuery.graphql"
 
-it("Artist3Heading (silly)", () => {
-  const props: Artist3HeadingProps = {
-    artist: {
-      name: "Andy Warhol",
-      birthYear: 123,
-      " $refType": "Artist3Heading_artist",
-    },
-  }
+describe("Artist3Heading", () => {
+  describe("as an isolated component", () => {
+    it("renders the values we give it", () => {
+      const props: Artist3HeadingProps = {
+        artist: {
+          name: "Andy Warhol",
+          birthYear: 123,
+          " $refType": "Artist3Heading_artist",
+        },
+      }
 
-  const { queryAllByText } = render(<Artist3Heading {...props} />)
+      const { queryAllByText } = render(<Artist3Heading {...props} />)
 
-  const header = queryAllByText("Andy Warhol")
-  expect(header).toHaveLength(1)
-})
-
-it("Artist3Heading", () => {
-  const mockEnvironment = createMockEnvironment()
-
-  const { queryAllByText } = render(
-    <QueryRenderer<Artist3HeadingTestQuery>
-      environment={mockEnvironment}
-      query={graphql`
-        query Artist3HeadingTestQuery($artistID: ID!) {
-          artist(id: $artistID) {
-            ...Artist3Heading_artist
-          }
-        }
-      `}
-      variables={{ artistID: "wow" }}
-      render={({ props }) => {
-        if (!props || !props.artist) {
-          return <div>Loading</div>
-        }
-        return <Artist3HeadingFragmentContainer artist={props.artist} />
-      }}
-    />
-  )
-
-  mockEnvironment.mock.resolveMostRecentOperation(operation =>
-    MockPayloadGenerator.generate(operation, {
-      Artist: () => ({
-        name: "Andy Warhol",
-        birthYear: 123,
-      }),
+      const header = queryAllByText("Andy Warhol")
+      expect(header).toHaveLength(1)
     })
-  )
+  })
 
-  const header = queryAllByText("Andy Warhol")
-  expect(header).toHaveLength(1)
+  describe("as a Relay fragmentContainer", () => {
+    it("renders the values from the Relay query", () => {
+      const mockEnvironment = createMockEnvironment()
+
+      const { queryAllByText } = render(
+        <QueryRenderer<Artist3HeadingTestQuery>
+          environment={mockEnvironment}
+          query={graphql`
+            query Artist3HeadingTestQuery($artistID: ID!) {
+              artist(id: $artistID) {
+                ...Artist3Heading_artist
+              }
+            }
+          `}
+          variables={{ artistID: "wow" }}
+          render={({ props }) => {
+            if (!props || !props.artist) {
+              return <div>Loading</div>
+            }
+            return <Artist3HeadingFragmentContainer artist={props.artist} />
+          }}
+        />
+      )
+
+      mockEnvironment.mock.resolveMostRecentOperation(operation =>
+        MockPayloadGenerator.generate(operation, {
+          Artist: () => ({
+            name: "Andy Warhol",
+            birthYear: 123,
+          }),
+        })
+      )
+
+      const header = queryAllByText("Andy Warhol")
+      expect(header).toHaveLength(1)
+    })
+  })
 })
