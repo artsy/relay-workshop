0baa2bcb78ffd0a442fa3ed87b83cb663d161b38
Steven Hicks <steven.j.hicks@gmail.com>
2021-08-27 11:17:45 -0500

    feat: fill in intro/setup/orientiation sections

---
+++ src/exercises/03-Testing-Queries/beginning/Artist3Heading.spec.tsx
@@ -0,0 +1,21 @@
+// import React from "react"
+// import { render } from "@testing-library/react"
+// import { QueryRenderer } from "react-relay"
+// import { MockPayloadGenerator, createMockEnvironment } from "relay-test-utils"
+// import { graphql } from "relay-runtime"
+// import {
+//   Artist3Heading,
+//   Artist3HeadingFragmentContainer,
+//   Artist3HeadingProps,
+// } from "./Artist3Heading"
+// import { Artist3HeadingTestQuery } from "./__generated__/Artist3HeadingTestQuery.graphql"
+
+// describe("Artist3Heading", () => {
+//   describe("as an isolated component", () => {
+//     it("renders the values we give it", () => {})
+//   })
+
+//   describe("as a Relay fragmentContainer", () => {
+//     it("renders the values from the Relay query", () => {})
+//   })
+// })
